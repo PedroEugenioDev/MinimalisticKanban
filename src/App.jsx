39a7a6a94584342a2f0dcd8ae1f6f917571a44aa
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-10-21 17:40:59 -0300

    Adding background images and adjusting responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <>
       <KanbanContextProvider>
-        <div className="screen">
+        <div className="screen" >
           <KanbanBoard />
         </div>
       </KanbanContextProvider>
