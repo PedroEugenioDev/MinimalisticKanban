2bf6ace15af8634967331a23cf1c917cc5dd3709
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-10-22 18:11:14 -0300

    Adding button to change theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import KanbanBoard from "./Components/KanbanBoard";
 import { KanbanContextProvider } from "./context/KanbanContext";
+import lightIcon from '/themes/light-theme-icon.svg';
+import darkIcon from '/themes/dark-theme-icon.svg';
 
 export default function App() {
+  const [theme, setTheme] = useState("light-theme")
+  const themeBtnHandler = () => {
+    if(theme == "light-theme"){
+      setTheme("dark-theme");
+    }else{
+      setTheme("light-theme")
+    }
+  }
+
   return (
     <>
       <KanbanContextProvider>
-        <div className="screen dark-theme" >
-          <button className="themeBtn">Trocar Tema</button>
+        <div className={`screen ${theme}`} >
+          <button className="themeBtn" onClick={themeBtnHandler}>
+            <img 
+              src={theme === "light-theme" ?  lightIcon : darkIcon} 
+              alt="Theme Icon" 
+              className="icon" 
+            />
+          </button>
           <KanbanBoard />
         </div>
       </KanbanContextProvider>
